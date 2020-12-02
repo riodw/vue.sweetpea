8d3f22efecf36502c83b5fefc6fee525d2c9773f
Rio Weber <riodweber@gmail.com>
2020-12-02 18:39:52 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from "vue";
+// router
+import VueRouter from "vue-router";
+// import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// lodash
+import VueLodash from "vue-lodash";
+// charts
+import VueApexCharts from "vue-apexcharts";
+
+// components
 import App from "./App.vue";
+
+import router from "./router";
 
 Vue.config.productionTip = false;
 
+// middle
+// Vue.use(require("vue-moment"));
+// Vue.use(VueAxios, axios);
+Vue.use(VueLodash);
+Vue.use(VueRouter);
+
+// components
+// Vue.component("VueCtkDateTimePicker", VueCtkDateTimePicker);
+Vue.component("apexchart", VueApexCharts);
+
 new Vue({
-  render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- <JumboTitle /> -->
+  </div>
+</template>
+
+<script>
+// import VueApexCharts from "vue-apexcharts";
+// components
+
+export default {
+  name: "Home",
+  components: {
+    // apexcharts: VueApexCharts,
+  },
+  data: function() {
+    return {};
+  },
+};
+</script>
