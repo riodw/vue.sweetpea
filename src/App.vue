41c482ad5b4a01c7dbcc546048e924e402465fa8
Rio Weber <riodweber@gmail.com>
2020-12-02 20:39:34 -0500

    add graph;

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="rounded overflow-hidden">
+        <img
+          class="rounded-circle"
+          alt="Vue logo"
+          src="./assets/logo.png"
+          style="width: 200px;"
+        />
+      </div>
+      <div class="jumbotron mt-3">
+        <h1>Bottom Navbar example</h1>
+        <p class="lead">
+          This example is a quick exercise to illustrate how the bottom navbar works.
+        </p>
+        <a
+          class="btn btn-lg btn-primary"
+          href="/docs/4.5/components/navbar/"
+          role="button"
+        >
+          View navbar docs &raquo;
+        </a>
+        <HelloWorld />
+      </div>
+      <Chart />
+    </div>
+    <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Bottom navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
+              >Disabled</a
+            >
+          </li>
+          <li class="nav-item dropup">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="dropdown10"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >Dropup</a
+            >
+            <div class="dropdown-menu" aria-labelledby="dropdown10">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Chart from "./components/Chart.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Chart,
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="chart">
+    <div>
+      <apexchart
+        type="line"
+        height="350"
+        :options="chartOptions"
+        :series="series"
+      ></apexchart>
+    </div>
+  </div>
+</template>
+
+<script>
+// import VueApexCharts from "vue-apexcharts";
+
+export default {
+  name: "Chart",
+  components: {
+    // apexcharts: VueApexCharts,
+  },
+  data: function() {
+    return {
+      series: [
+        {
+          name: "Likes",
+          data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5],
+        },
+      ],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: "line",
+        },
+        stroke: {
+          width: 7,
+          curve: "smooth",
+        },
+        xaxis: {
+          type: "datetime",
+          categories: [
+            "1/11/2000",
+            "2/11/2000",
+            "3/11/2000",
+            "4/11/2000",
+            "5/11/2000",
+            "6/11/2000",
+            "7/11/2000",
+            "8/11/2000",
+            "9/11/2000",
+            "10/11/2000",
+            "11/11/2000",
+            "12/11/2000",
+            "1/11/2001",
+            "2/11/2001",
+            "3/11/2001",
+            "4/11/2001",
+            "5/11/2001",
+            "6/11/2001",
+          ],
+          tickAmount: 10,
+          labels: {
+            formatter: function(value, timestamp, opts) {
+              return opts.dateFormatter(new Date(timestamp), "dd MMM");
+            },
+          },
+        },
+        title: {
+          text: "Social Media",
+          align: "left",
+          style: {
+            fontSize: "16px",
+            color: "#666",
+          },
+        },
+        fill: {
+          // colors: ["#1A73E8", "#B32824"],
+          type: "gradient",
+          gradient: {
+            // shade: "dark",
+            // gradientToColors: ["#ffea00", "#ff9ea4"],
+            shadeIntensity: 1,
+            type: "horizontal",
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100],
+            colorStops: [
+              {
+                offset: 0,
+                color: "#ffea00",
+                opacity: 1,
+              },
+              {
+                offset: 100,
+                color: "#ff9ea4",
+                opacity: 1,
+              },
+            ],
+          },
+        },
+        markers: {
+          size: 4,
+          colors: ["#ff9ea4"],
+          strokeColors: "#fff",
+          strokeWidth: 2,
+          hover: {
+            size: 7,
+          },
+        },
+        yaxis: {
+          min: -10,
+          max: 40,
+          title: {
+            text: "Engagement",
+          },
+        },
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
