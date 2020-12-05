f4b5a552671ff6012163546e79a3af8a4ec9a3a1
Rio Weber <riodweber@gmail.com>
2020-12-04 22:01:44 -0500

    gh-pages

---
+++ js/chunk-2d21a3d2.5ac06b54.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"home"})},c=[],o={name:"Home",components:{},data:function(){return{}}},s=o,u=t("2877"),l=Object(u["a"])(s,a,c,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.5ac06b54.js.map
